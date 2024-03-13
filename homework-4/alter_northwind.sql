5b11337f816c1d4c2d4bce4ad7e70ea719ef4bd3
ro_k_ <ro_k_sana@mail.ru>
2024-03-13 17:04:07 +0500

    DZ 5 курс: homework-4. Data Definition Language

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,63 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
+ALTER TABLE PRODUCTS ADD CONSTRAINT UNIT_PRICE CHECK (UNIT_PRICE > 0);
+
+--проверка
+UPDATE products SET unit_price = 0 WHERE product_id=1
+--ERROR:  Ошибочная строка содержит (1, Chai, 8, 1, 10 boxes x 30 bags, 0, 39, 0, 10, 1).новая строка в отношении "products" нарушает ограничение-проверку "unit_price"
+--ОШИБКА:  новая строка в отношении "products" нарушает ограничение-проверку "unit_price"
+--SQL-состояние: 23514
+--Подробности: Ошибочная строка содержит (1, Chai, 8, 1, 10 boxes x 30 bags, 0, 39, 0, 10, 1).
 
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
+ALTER TABLE products ADD CONSTRAINT discontinued CHECK (discontinued IN (0, 1));
+
+--проверка
+UPDATE products SET discontinued = 3 WHERE product_id=1
+--ERROR:  Ошибочная строка содержит (1, Chai, 8, 1, 10 boxes x 30 bags, 18, 39, 0, 10, 3).новая строка в отношении "products" нарушает ограничение-проверку "discontinued"
+--ОШИБКА:  новая строка в отношении "products" нарушает ограничение-проверку "discontinued"
+--SQL-состояние: 23514
+--Подробности: Ошибочная строка содержит (1, Chai, 8, 1, 10 boxes x 30 bags, 18, 39, 0, 10, 3).
 
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
+SELECT * INTO discontinued_products FROM products WHERE discontinued = 1;
+
+--проверки
+SELECT table_catalog, table_schema, table_name FROM information_schema.tables WHERE table_name = 'discontinued_products'
+--"table_catalog","table_schema","table_name"
+--"northwind","public","discontinued_products"
+SELECT * FROM discontinued_products
+--"product_id","product_name","supplier_id","category_id","quantity_per_unit","unit_price","units_in_stock","units_on_order","reorder_level","discontinued"
+--1,"Chai",8,1,"10 boxes x 30 bags","18",39,0,10,1
+--2,"Chang",1,1,"24 - 12 oz bottles","19",17,40,25,1
+--5,"Chef Anton's Gumbo Mix",2,2,"36 boxes","21.35",0,0,0,1
+--9,"Mishi Kobe Niku",4,6,"18 - 500 g pkgs.","97",29,0,0,1
+--17,"Alice Mutton",7,6,"20 - 1 kg tins","39",0,0,0,1
+--24,"Guaraná Fantástica",10,1,"12 - 355 ml cans","4.5",20,0,0,1
+--28,"Rössle Sauerkraut",12,7,"25 - 825 g cans","45.6",26,0,0,1
+--29,"Thüringer Rostbratwurst",12,6,"50 bags x 30 sausgs.","123.79",0,0,0,1
+--42,"Singaporean Hokkien Fried Mee",20,5,"32 - 1 kg pkgs.","14",26,0,0,1
+--53,"Perth Pasties",24,6,"48 pieces","32.8",0,0,0,1
 
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
--- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+-- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key.
+-- Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+
+--проверка перед удалением
+SELECT COUNT(*) FROM products
+--"count"
+--"77"
+
+--снятие ограничения на внешний ключ
+ALTER TABLE order_details DROP CONSTRAINT fk_order_details_products;
+--удаление из products товаров, снятых с продажи (discontinued = 1)
+DELETE FROM products WHERE discontinued = 1
+
+--проверка после удаления
+SELECT COUNT(*) FROM products
+--"count"
+--"67"
+
--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,105 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
+--import psycopg2
+--conn_params = psycopg2.connect(host="127.0.0.1", database="Northwind", user="postgres", password="bd_pyt", client_encoding="utf-8")
+--cursor = conn_params.cursor()
+--conn_params.autocommit = True
+--cursor.execute("CREATE TABLE student (student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar)")
+--conn_params.close()
 
+CREATE TABLE student
+(student_id serial,
+first_name varchar,
+last_name varchar,
+birthday date,
+phone varchar
+);
+
+--проверка
+SELECT table_catalog, table_schema, table_name FROM information_schema.tables WHERE table_name = 'student'
+--"table_catalog","table_schema","table_name"
+--"northwind","public","student"
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
+ALTER TABLE student ADD COLUMN middle_name varchar;
+
+--проверка
+SELECT column_name, column_default, data_type FROM INFORMATION_SCHEMA. WHERE table_name = 'student';
+--"column_name","column_default","data_type"
+--"student_id","nextval('student_student_id_seq'::regclass)","integer"
+--"first_name",NULL,"character varying"
+--"last_name",NULL,"character varying"
+--"birthday",NULL,"date"
+--"phone",NULL,"character varying"
+--***"middle_name",NULL,"character varying"***
 
 
 -- 3. Удалить колонку middle_name
+ALTER TABLE student DROP COLUMN middle_name;
+
+--проверка
+SELECT column_name, column_default, data_type FROM INFORMATION_SCHEMA. WHERE table_name = 'student';
+--"column_name","column_default","data_type"
+--"student_id","nextval('student_student_id_seq'::regclass)","integer"
+--"first_name",NULL,"character varying"
+--"last_name",NULL,"character varying"
+--"birthday",NULL,"date"
+--"phone",NULL,"character varying"
 
 
 -- 4. Переименовать колонку birthday в birth_date
+ALTER TABLE student RENAME birthday TO birth_date;
+
+--проверка
+SELECT column_name, column_default, data_type FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'student';
+--"column_name","column_default","data_type"
+--"student_id","nextval('student_student_id_seq'::regclass)","integer"
+--"first_name",NULL,"character varying"
+--"last_name",NULL,"character varying"
+--***"birth_day",NULL,"date"***
+--"phone",NULL,"character varying"
 
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32);
+
+--проверка до изменений
+SELECT column_name, column_default, data_type, character_maximum_length FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'student';
+--"column_name","column_default","data_type","character_maximum_length"
+--"student_id","nextval('student_student_id_seq'::regclass)","integer",NULL
+--"first_name",NULL,"character varying",NULL
+--"last_name",NULL,"character varying",NULL
+--"birth_date",NULL,"date",NULL
+--***"phone",NULL,"character varying",NULL***
+--проверка после изменений
+SELECT column_name, column_default, data_type, character_maximum_length FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'student';
+--"column_name","column_default","data_type","character_maximum_length"
+--"student_id","nextval('student_student_id_seq'::regclass)","integer",NULL
+--"first_name",NULL,"character varying",NULL
+--"last_name",NULL,"character varying",NULL
+--"birth_date",NULL,"date",NULL
+--***"phone",NULL,"character varying",32***
 
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
+INSERT INTO student
+(first_name, last_name, birth_date, phone)
+VALUES
+('Первый', 'Студент', '2000-11-03', '+79991234578'),
+('Вторая', 'Студентка', '1995-10-21', '+79993692514'),
+('Третий', 'Курсант', '2002-05-07', '+79992581245');
+
+--проверка
+ select * from student
+--"student_id","first_name","last_name","birth_date","phone"
+--1,"Первый","Студент","2000-11-03","+79991234578"
+--2,"Вторая","Студентка","1995-10-21","+79993692514"
+--3,"Третий","Курсант","2002-05-07","+79992581245"
 
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY;
+
+--проверка
+ select count(*) from student
+--"count"
+--"0"
